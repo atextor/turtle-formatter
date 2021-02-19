bfa53c7fc0844f9a9009c99c4163a00753e1feba
Andreas Textor <textor.andreas@googlemail.com>
2021-02-19 06:15:57 +0100

    Write Jacoco HTML report

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,6 +76,8 @@ tasks {
         reports {
             xml.isEnabled = true
             xml.destination = file("${buildDir}/reports/jacoco/report.xml")
+            html.isEnabled = true
+            html.destination = file("${buildDir}/reports/jacoco/html-report")
         }
     }
 }
