3c74c706c0071b4e15672583118136463aca1385
Andreas Textor <textor.andreas@googlemail.com>
2021-02-26 06:02:16 +0100

    Configure build for performing releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,13 @@ plugins {
     id("com.github.ben-manes.versions") version "0.36.0"
     id("com.adarshr.test-logger") version "2.1.1"
     id("io.franzbecker.gradle-lombok") version "4.0.0"
+    `java-library`
+    `maven-publish`
+    signing
 }
+
+group = "de.atextor"
+description = "Library for pretty printing RDF/Turtle documents"
 
 repositories {
     mavenCentral()
@@ -28,8 +34,9 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 }
 
-jacoco {
-    toolVersion = "0.8.6"
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 fun isNonStable(version: String): Boolean {
@@ -49,37 +56,107 @@ tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs.addAll(arrayOf("--release", "11"))
 }
 
-tasks {
-    compileJava {
-        options.encoding = "UTF-8"
-        sourceCompatibility = "15"
-        targetCompatibility = "11"
-    }
+tasks.compileJava {
+    options.encoding = "UTF-8"
+    sourceCompatibility = "15"
+    targetCompatibility = "11"
+}
 
-    compileTestJava {
-        options.encoding = "UTF-8"
-        sourceCompatibility = "15"
-        targetCompatibility = "11"
-    }
+tasks.compileTestJava {
+    options.encoding = "UTF-8"
+    sourceCompatibility = "15"
+    targetCompatibility = "11"
+}
 
-    test {
-        useJUnitPlatform()
-        ignoreFailures = false
-        failFast = true
-    }
+tasks.test {
+    useJUnitPlatform()
+    ignoreFailures = false
+    failFast = true
+    maxHeapSize = "1G"
+}
 
-    javadoc {
-        (options as CoreJavadocOptions).addStringOption("Xdoclint:accessibility,html,syntax,reference", "-quiet")
-        options.encoding = "UTF-8"
-        shouldRunAfter(test)
-    }
+jacoco {
+    toolVersion = "0.8.6"
+}
 
-    jacocoTestReport {
-        reports {
-            xml.isEnabled = true
-            xml.destination = file("${buildDir}/reports/jacoco/report.xml")
-            html.isEnabled = true
-            html.destination = file("${buildDir}/reports/jacoco/html-report")
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        xml.destination = file("${buildDir}/reports/jacoco/report.xml")
+        html.isEnabled = true
+        html.destination = file("${buildDir}/reports/jacoco/html-report")
+    }
+}
+
+tasks.javadoc {
+    (options as CoreJavadocOptions).addStringOption("Xdoclint:accessibility,html,syntax,reference", "-quiet")
+    options.encoding = "UTF-8"
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+    shouldRunAfter(tasks.test)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "turtle-formatter"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("turtle-formatter")
+                description.set(project.description)
+                url.set("https://github.com/atextor/turtle-formatter")
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public Public License Version 3")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("atextor")
+                        name.set("Andreas Textor")
+                        email.set("mail@atextor.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/atextor/turtle-formatter.git")
+                    developerConnection.set("scm:git:ssh://github.com:atextor/turtle-formatter.git")
+                    url.set("https://github.com/atextor/turtle-formatter/tree/main")
+                }
+                issueManagement {
+                    url.set("https://github.com/atextor/turtle-formatter/issues")
+                    system.set("GitHub")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
         }
     }
 }
+
+// This configuration will read the signing PGP key and password from environment variables
+// ORG_GRADLE_PROJECT_signingKey (in ASCII-armored format) and ORG_GRADLE_PROJECT_signingPassword
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["mavenJava"])
+}
+
